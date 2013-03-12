09aa60db572114c6fe59391c7bbbcb53e9fedee9
leinfelder <leinfelder@nceas.ucsb.edu>
2013-03-12 15:51:25 -0700

    include possible values for datacite.resourcetype

--- src/main/java/edu/ucsb/nceas/ezid/profile/DataCiteProfile.java
+++ src/main/java/edu/ucsb/nceas/ezid/profile/DataCiteProfile.java
@@ -2,7 +2,7 @@ package edu.ucsb.nceas.ezid.profile;
 
 /**
  * EZID profile for DataCite metadata.
- * Specifies the supported metadata elements.
+ * Specifies the supported metadata elements and possible values for them.
  * @see http://n2t.net/ezid/doc/apidoc.html#profile-datacite
  * @author leinfelder
  *
@@ -12,7 +12,20 @@ public enum DataCiteProfile {
 	TITLE("title"),
 	PUBLISHER("publisher"), 
 	PUBLICATION_YEAR("publicationyear"), 
-	RESOURCE_TYPE("resourcetype");
+	RESOURCE_TYPE("resourcetype"),
+	//resourcetype values
+	COLLECTION("Collection"),
+	DATASET("Dataset"),
+	EVENT("Event"),
+	FILM("Film"),
+	IMAGE("Image"),
+	INTERACTIVE_RESOURCE("InteractiveResource"),
+	MODEL("Model"),
+	PHYSICAL_OBJECT("PhysicalObject"),
+	SERVICE("Service"),
+	SOFTWARE("Software"),
+	SOUND("Sound"),
+	TEXT("Text");
 	
 	private static final String profileName = "datacite";
 	
